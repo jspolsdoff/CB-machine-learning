418753a8a66153634f8a1437e50a3cb090423472
James Spolsdoff <jspolsdoff@gmail.com>
2019-01-25 08:51:22 -0800

    init commit

---
+++ main.m
@@ -0,0 +1,11 @@
+% Cyclebar Machine Learning Script
+%
+% using multivariant gradient descent predict cycle points
+
+%% Clear and Close Figures
+clear ; close all; clc
+
+fprintf('Loading data ...\n');
+
+%% Load Data
+
