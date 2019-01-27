235de20274ea8351d99798ec6cd5f02c16f0d886
James Spolsdoff <jspolsdoff@gmail.com>
2019-01-27 08:59:30 -0800

    plan out main script

--- main.m
+++ main.m
@@ -7,5 +7,16 @@ clear ; close all; clc
 
 fprintf('Loading data ...\n');
 
-%% Load Data
+% Load Data
 
+% Scale features and set them to zero mean
+
+% Add intercept term to X
+
+% Choose some alpha value
+
+% Init Theta and Run Gradient Descent 
+
+% Plot the convergence graph
+
+% Display gradient descent's result
